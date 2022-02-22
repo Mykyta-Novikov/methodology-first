67bf20121f3890aeb52c02672326be2210bfb656
Mykyta-Novikov <novikov20021201@gmail.com>
2022-02-22 05:07:07 +0200

    fix: handle NoSuchElementException during reading, and properly close file after that

--- src/java/equation/Main.java
+++ src/java/equation/Main.java
@@ -3,10 +3,13 @@ package equation;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public final class Main {
+    private static final int INVALID_FORMAT_EXIT_STATUS = 2;
+    private static final int FILE_NOT_FOUND_EXIT_STATUS = 1;
+
     public static void main(String[] args) {
         InputStream input;
         if (args.length >= 1)
@@ -14,19 +17,25 @@ public final class Main {
                 input = new FileInputStream(args[0]);
             } catch (FileNotFoundException e) {
                 System.err.println("File not found");
-                System.exit(1);
+                System.exit(FILE_NOT_FOUND_EXIT_STATUS);
                 return;
             }
         else
             input = System.in;
 
-        var scanner = new Scanner(input);
+        Equation equation;
+        try (var scanner = new Scanner(input)) {
+            double  a = scanner.nextDouble(),
+                    b = scanner.nextDouble(),
+                    c = scanner.nextDouble();
 
-        double  a = scanner.nextDouble(),
-                b = scanner.nextDouble(),
-                c = scanner.nextDouble();
-
-        var equation = new Equation(a, b, c);
+            equation = new Equation(a, b, c);
+        }
+        catch (NoSuchElementException e) {
+            System.err.println("Invalid input format");
+            System.exit(INVALID_FORMAT_EXIT_STATUS);
+            return;
+        }
 
         System.out.println("\n" + equation);
         System.out.println(equation.solve());
